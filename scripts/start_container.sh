8d9312ea976c006478d5d4d464c6d5a380bbf9d3
practaws31git <practaws31@gmail.com>
2025-09-10 17:12:54 +0530

    update the files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull practaws/simple-python-project-code:v1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 practaws/simple-python-project-code:v1
